3ebfb93580fbe8b10020740e643303dc161cc26d
The1024thmember <1786728683@qq.com>
2022-09-24 16:01:41 +1000

    fixed column issue, but two column can not be both 6

--- paintingProject/src/app/LandingPage/LandingPage-Objective/landingPage-objective.component.ts
+++ paintingProject/src/app/LandingPage/LandingPage-Objective/landingPage-objective.component.ts
@@ -1,21 +1,21 @@
-import { Component } from "@angular/core";
-import { HeadingType } from "src/app/ComponentLibrary/MyHeading";
+import { Component } from '@angular/core';
+import { HeadingType } from 'src/app/ComponentLibrary/MyHeading';
 
 @Component({
-    selector: "landingPage-Objectives",
-    template:`
-    <my-container>
-        <my-grid>
-            <my-col [col]="8">
-                <my-heading [headingType] = 'HeadingType.H1'>"The Art of Togetherness"</my-heading>
-            </my-col>
-            <my-col [col]="2">
-                <my-heading [headingType] = 'HeadingType.H1'>"2"</my-heading>
-            </my-col>
-        </my-grid>
-    </my-container>`,
+  selector: 'landingPage-Objectives',
+  template: ` <my-container>
+    <my-grid>
+      <my-col [colDesktopSmall]="6">
+        <my-heading [headingType]="HeadingType.H1"
+          >The Art of Togetherness</my-heading
+        >
+      </my-col>
+      <my-col [colDesktopSmall]="5" [colTablet]="12">
+        <my-heading [headingType]="HeadingType.H1">2</my-heading>
+      </my-col>
+    </my-grid>
+  </my-container>`,
 })
-
 export class LandingPageObjectiveComponent {
-    HeadingType = HeadingType;
+  HeadingType = HeadingType;
 }
