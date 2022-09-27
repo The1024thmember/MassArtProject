296e39cd6d481f7db74ac4f919b0d1b3be4b5b23
The1024thmember <1786728683@qq.com>
2022-09-27 21:55:57 +1000

    make import icon working

--- paintingProject/src/app/DrawBoardPage/DrawBoardPage-tools/DrawBoardPage-tools.ts
+++ paintingProject/src/app/DrawBoardPage/DrawBoardPage-tools/DrawBoardPage-tools.ts
@@ -3,12 +3,47 @@ import { HeadingType } from 'src/app/ComponentLibrary/MyHeading';
 
 @Component({
   selector: 'drawBoardPage-tools',
-  template: ` <my-container>
+  template: ` <my-container class="Tools">
     <my-grid>
-      <my-col [col]="2"> </my-col>
-      <my-col [col]="2"> </my-col>
+      <my-col [col]="3"> Mass Art </my-col>
+      <my-col [col]="6">
+        <my-grid>
+          <my-col [col]="2"> line </my-col>
+          <my-col [col]="2"> curve</my-col>
+          <my-col [col]="2">
+            <button>
+              <i class="bi bi-square"></i>
+            </button>
+          </my-col>
+          <my-col [col]="2">
+            <button>
+              <i class="bi bi-circle"></i>
+            </button>
+          </my-col>
+          <my-col [col]="2">
+            <button>
+              <i class="bi bi-triangle"></i>
+            </button>
+          </my-col>
+        </my-grid>
+      </my-col>
+      <my-col [col]="3">
+        <my-grid>
+          <my-col [col]="2">
+            <button>
+              <i class="bi bi-border-width"></i>
+            </button>
+          </my-col>
+          <my-col [col]="2">
+            <button>
+              <i class="bi bi-paint-bucket"></i>
+            </button>
+          </my-col>
+        </my-grid>
+      </my-col>
     </my-grid>
   </my-container>`,
+  styleUrls: ['./DrawBoardPage-tools.scss'],
 })
 export class DrawBoardToolsComponent {
   HeadingType = HeadingType;
