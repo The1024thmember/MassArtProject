53f6dabafce075c18b32bf4666f7cfb5564427c7
The1024thmember <1786728683@qq.com>
2022-10-11 23:20:38 +1100

    make the layout of the drawboard page tindy

--- paintingProject/src/app/DrawBoardPage/DrawBoardPage.component.ts
+++ paintingProject/src/app/DrawBoardPage/DrawBoardPage.component.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
+import { Margin } from '../Directives/Margin';
 
 @Component({
   template: `
-    <drawBoardPage-tools></drawBoardPage-tools>
-    <drawBoardPage-controls></drawBoardPage-controls>
-    <drawBoardPage-userPanel></drawBoardPage-userPanel>
-    <drawBoardPage-colorPlatte></drawBoardPage-colorPlatte>
+    <my-container class="Container">
+      <my-grid class="Container-firstRowTools">
+        <my-col [col]="5">
+          <drawBoardPage-tools></drawBoardPage-tools>
+        </my-col>
+        <my-col [col]="5">
+          <drawBoardPage-controls></drawBoardPage-controls>
+        </my-col>
+        <my-col [col]="2">
+          <drawBoardPage-userPanel></drawBoardPage-userPanel>
+        </my-col>
+      </my-grid>
+      <drawBoardPage-colorPlatte></drawBoardPage-colorPlatte>
+    </my-container>
   `,
+  styleUrls: ['./DrawBoardPage.scss'],
 })
-export class DrawBoardPageComponent {}
+export class DrawBoardPageComponent {
+  Margin = Margin;
+}
